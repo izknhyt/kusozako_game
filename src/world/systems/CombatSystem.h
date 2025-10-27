16430cc0b2a250fc478b30f294a41838e3d510bc
izknhyt <nemutesu@gmail.com>
2025-10-27 18:29:02 +0900

    Introduce spatial grid for combat collisions

--- src/world/systems/CombatSystem.h
+++ src/world/systems/CombatSystem.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "world/SpatialGrid.h"
 #include "world/systems/SystemContext.h"
+
+#include <cstdint>
+#include <vector>
 
 namespace world::systems
 {
@@ -11,6 +15,16 @@ class CombatSystem : public ISystem
     CombatSystem() = default;
 
     void update(float dt, SystemContext &context) override;
+
+  private:
+    SpatialGrid m_grid;
+    std::vector<std::uint32_t> m_enemyVisit;
+    std::vector<std::uint32_t> m_wallVisit;
+    std::uint32_t m_enemyStamp = 1;
+    std::uint32_t m_wallStamp = 1;
+    std::vector<std::size_t> m_cellScratch;
+    std::vector<std::size_t> m_enemyScratch;
+    std::vector<std::size_t> m_wallScratch;
 };
 
 } // namespace world::systems
